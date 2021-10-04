fa4f63e6d4f17c918ccb55c74ce1d9d60d716286
chehin <chehinjuan@gmail.com>
2021-10-04 10:03:37 -0300

    it en App.test

--- testing/src/components/__test__/App.test.js
+++ testing/src/components/__test__/App.test.js
@@ -1,3 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
+
+it('shows a comment box', () => {
+    const div = document.create('div'); // div 'falso' para poder ejecutar pruebas sin el navegador
+
+    ReactDOM.render(<App/>,div);
+
+    ReactDOM.unmountComponentAtNode(div);
+});
