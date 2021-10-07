29e582d455946c0199d63f6b85c289e0188bbce3
chehin <chehinjuan@gmail.com>
2021-10-07 09:53:06 -0300

    Refactorizacion de app.test + correccion de PATH

--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -1,12 +1,17 @@
-import react from 'react';
-import CommentBox from './CommentBox';
-import CommentList from './CommentList';
+import React, { Component } from 'react';
 
-export default () => {
-    return (
-        <div>
-            <CommentBox />
-            <CommentList />
-        </div>
-    );
-};
+class CommentBox extends Component {
+    render(){
+        return (
+            <form>
+                <h4>Add a Comment</h4>
+                <textarea />
+                <div>
+                    <button>Submit Comment</button>
+                </div>
+            </form>
+        );
+    }
+}
+
+export default CommentBox;
