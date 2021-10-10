cfccb18dabaff93149826a71480988c53e2068d2
chehin <chehinjuan@gmail.com>
2021-10-10 18:23:28 -0300

    beforeEach y afterEach + it para text area

--- testing/src/components/__test__/CommentBox.test.js
+++ testing/src/components/__test__/CommentBox.test.js
@@ -2,9 +2,24 @@ import React from 'react';
 import { mount } from 'enzyme';
 import CommentBox from '../CommentBox';
 
-it('has a text area and button', () => {
-    const wrapper = mount(<CommentBox />);
+let wrapper;
 
+beforeEach(() => {
+    wrapper = mount(<CommentBox />);
+});
+
+afterEach(() => {
+    wrapper.unmount();
+});
+
+it('has a text area and button', () => {
     expect(wrapper.find('textarea').length).toEqual(1);
     expect(wrapper.find('button').length).toEqual(1);
+});
+
+it('has a text area that users can type in', () => {
+    wrapper.find('textarea').simulate('change', {
+        target: { value: 'new comment' }
+    });
+    wrapper.update();
 });
