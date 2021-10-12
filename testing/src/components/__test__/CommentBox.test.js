4c05374352088558c7aeb54ed586c4263300e85f
chehin <chehinjuan@gmail.com>
2021-10-12 10:42:01 -0300

    Bloque describe

--- testing/src/components/__test__/CommentBox.test.js
+++ testing/src/components/__test__/CommentBox.test.js
@@ -14,34 +14,40 @@ afterEach(() => {
     wrapped.unmount();
 });
 
-it('has a text area and button', () => {
-    expect(wrapped.find('textarea').length).toEqual(1);
-    expect(wrapped.find('button').length).toEqual(1);
-});
 
-it('has a text area that users can type in', () => {
-    wrapped.find('textarea').simulate('change', {
-        target: { value: 'new comment' }
-    });
-    wrapped.update();
-
-    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
-});
-
-// Prueba que el area de texto se vacie cuando se haga clic en el boton enviar
-// Debemos simular un evento en el propio formulario
-it('Text area empty before the clic', () => {
-    // Cargo un valor al textarea
-    wrapped.find('textarea').simulate('change', {
-        target: { value: 'new comment' }
+describe('the text area', () => {
+    beforeEach(() => {
+        wrapped.find('textarea').simulate('change', {
+            target: { value: 'new comment' }
+        });
+        wrapped.update();
     });
 
-    wrapped.update();
+    it('has a text area and button', () => {
+        expect(wrapped.find('textarea').length).toEqual(1);
+        expect(wrapped.find('button').length).toEqual(1);
+    });
 
-    // Verifico el cambio en el form
-    wrapped.find('form').simulate('submit');
-    
-    wrapped.update();
-    // Chequeo que el textarea este vacio
-    expect(wrapped.find('textarea').prop('value')).toEqual('');
+    it('has a text area that users can type in', () => {
+        expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+    });
+
+    // Prueba que el area de texto se vacie cuando se haga clic en el boton enviar
+    // Debemos simular un evento en el propio formulario
+    it('Text area empty before the clic', () => {
+        // Cargo un valor al textarea
+        wrapped.find('textarea').simulate('change', {
+            target: { value: 'new comment' }
+        });
+
+        wrapped.update();
+
+        // Verifico el cambio en el form
+        wrapped.find('form').simulate('submit');
+        
+        wrapped.update();
+        // Chequeo que el textarea este vacio
+        expect(wrapped.find('textarea').prop('value')).toEqual('');
+    });
+
 });
