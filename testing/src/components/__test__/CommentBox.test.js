c2bb2f038de82a9a0e2d8602abbc51119b7ed15e
chehin <chehinjuan@gmail.com>
2021-10-12 10:15:21 -0300

    Testeo de caja vacia al enviar el form

--- testing/src/components/__test__/CommentBox.test.js
+++ testing/src/components/__test__/CommentBox.test.js
@@ -2,24 +2,46 @@ import React from 'react';
 import { mount } from 'enzyme';
 import CommentBox from '../CommentBox';
 
-let wrapper;
+let wrapped;
 
 beforeEach(() => {
-    wrapper = mount(<CommentBox />);
+    console.log('pasa beforeEach');
+    wrapped = mount(<CommentBox />);
 });
 
 afterEach(() => {
-    wrapper.unmount();
+    console.log('pasa afterEach');
+    wrapped.unmount();
 });
 
 it('has a text area and button', () => {
-    expect(wrapper.find('textarea').length).toEqual(1);
-    expect(wrapper.find('button').length).toEqual(1);
+    expect(wrapped.find('textarea').length).toEqual(1);
+    expect(wrapped.find('button').length).toEqual(1);
 });
 
 it('has a text area that users can type in', () => {
-    wrapper.find('textarea').simulate('change', {
+    wrapped.find('textarea').simulate('change', {
         target: { value: 'new comment' }
     });
-    wrapper.update();
+    wrapped.update();
+
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+});
+
+// Prueba que el area de texto se vacie cuando se haga clic en el boton enviar
+// Debemos simular un evento en el propio formulario
+it('Text area empty before the clic', () => {
+    // Cargo un valor al textarea
+    wrapped.find('textarea').simulate('change', {
+        target: { value: 'new comment' }
+    });
+
+    wrapped.update();
+
+    // Verifico el cambio en el form
+    wrapped.find('form').simulate('submit');
+    
+    wrapped.update();
+    // Chequeo que el textarea este vacio
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
 });
