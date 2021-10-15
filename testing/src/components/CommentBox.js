a379fe9bec70f406d26eb1062780617ee808ee01
chehin <chehinjuan@gmail.com>
2021-10-15 09:59:40 -0300

    Creacion del componente Root y remmplazos correspondietes + save comment

--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from 'actions';
 
 class CommentBox extends Component {
     state = { comment: ''};
@@ -10,7 +12,8 @@ class CommentBox extends Component {
     handleSubmit = event => {
         event.preventDefault();
 
-        this.setState({ comment: ''});
+        this.props.saveComment(this.state.comment);
+        this.setState({ comment: '' });
     };
 
     render(){
@@ -26,4 +29,4 @@ class CommentBox extends Component {
     }
 }
 
-export default CommentBox;
+export default connect(null,actions)(CommentBox);
